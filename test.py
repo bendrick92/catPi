bd741fda835b173eebd8ce5b4cdc735d4075b55e
Ben Walters <Ben Walters>
2016-11-20 17:35:54 -0600

    Updated logging messages, added additional (basic) exception handling

--- test.py
+++ test.py
@@ -11,27 +11,35 @@ with open('schedule.json') as schedule_file:
 for i in schedule_data['events']:
     id = i['id']
     feed_amount = i['feed_amount']
-    event_time = i['event_time']
+    event_time = datetime.strptime(i['event_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
     has_run = i['has_run']
 
+    print('Evaluating event ' + id + '...')
     if has_run == 'false' and event_time <= current_timestamp:
         print('Executing event ' + id + '...')
         try:
             # TODO: Run event 
             i['has_run'] = 'true'
-            print('Event ' + id + ' executed successfully')
+            print('Event ' + id + ' executed successfully!')
         except:
-            print('An error occurred; skipping event ' + id + '...')
+            print('An error occurred - Skipping event ' + id + '...')
     else:
         skip_message = 'Skipping event ' + id + ' - '
         if has_run == 'true':
-            skip_message += 'Has already been run'
+            skip_message += 'Has already been run!'
         elif event_time > current_timestamp:
-            skip_message += 'Set to run at ' + event_time
+            skip_message += 'Set to run at ' + str(event_time)
 
         print(skip_message)
 
-with open('schedule.json', 'w+') as schedule_file:
-    schedule_file.write(json.dumps(schedule_data))
-    schedule_file.close()
+print('EOF reached')
+print('Writing changes...')
 
+try:
+    with open('schedule.json', 'w+') as schedule_file:
+        schedule_file.write(json.dumps(schedule_data))
+        schedule_file.close()
+
+    print('Changes written to file successfully!')
+except:
+    print('An error occurred - Changes not written to file')
